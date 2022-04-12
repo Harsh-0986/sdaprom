d69f5c322cd012533cee9174334b0004ee4999f5
Harsh-0986 <harsh.r.shah2102@gmail.com>
2022-04-12 18:53:22 +0530

    Second deploy

--- components/Details.jsx
+++ components/Details.jsx
@@ -14,7 +14,7 @@ const Details = () => {
 						/>
 						<p className="leading-relaxed mt-5">
 							Edison bulb retro cloud bread echo park, helvetica
-							stumptown taiyaki taxidermy 90's cronut +1 kinfolk.
+							stumptown taiyaki taxidermy 90s cronut +1 kinfolk.
 							Single-origin coffee ennui shaman taiyaki vape DIY
 							tote bag drinking vinegar cronut adaptogen squid
 							fanny pack vaporware.
