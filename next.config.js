a4fba3ac83a42b259b6e69715f73d0847adcb07d
Harsh-0986 <harsh.r.shah2102@gmail.com>
2022-04-12 18:42:02 +0530

    first deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['instagram.famd1-3.fna.fbcdn.net']
+  }
 }
 
 module.exports = nextConfig
